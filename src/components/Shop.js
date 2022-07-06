4d8e3d3e9370cfdaee4dfc2b2daa978ac22ad267
Akash <akashuhulekal@gmail.com>
2022-07-06 22:50:17 +0530

    started with redux frontend completed

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>buy shoes</h2>
+   <button className="btn btn-primary">-</button>
+   Add this item?
+   <button className="btn btn-primary">+</button>
+   </div>
+  )
+}
+
+export default Shop
